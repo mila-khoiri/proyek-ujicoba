9b2c0307184350ad00a50640495c586fb9584e0d
mila-khoiri <isnaamilatul24@gmail.com>
2026-09-05 22:21:17 +0700

    Fix static files route for Vercel

--- server.js
+++ server.js
@@ -24,3 +24,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server runningon port ${PORT}`);
 });
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
