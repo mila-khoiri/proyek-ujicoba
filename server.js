9ac91794db7846af9b4f561f348982f425bc62ed
mila-khoiri <isnaamilatul24@gmail.com>
2026-09-09 23:44:26 +0700

    Fix wildcard route syntax for Express

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/auth', authRoutes);
 
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
