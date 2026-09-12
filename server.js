d1086d8231cb515f3495c92d74df2fe502475843
mila-khoiri <isnaamilatul24@gmail.com>
2026-09-12 22:18:19 +0700

    Fix 404 with Express regex route handler

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/auth', authRoutes);
 
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 if(process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
